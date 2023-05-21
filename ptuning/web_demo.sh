2a716ed56c1259140ff112b8eb73393e1030b53b
yanlhou <>
2023-05-21 09:12:23 +0800

    no message

--- ptuning/web_demo.sh
+++ ptuning/web_demo.sh
@@ -1,7 +1,7 @@
 PRE_SEQ_LEN=128
-
+MODEL=/home/user/imported_models/chatglm-6b-20230419
+CHECKPOINT=output/kefu-128-1e-2-64-512/checkpoint-3000
 CUDA_VISIBLE_DEVICES=0 python3 web_demo.py \
-    --model_name_or_path THUDM/chatglm-6b \
-    --ptuning_checkpoint output/adgen-chatglm-6b-pt-128-2e-2/checkpoint-3000 \
+    --model_name_or_path $MODEL \
+    --ptuning_checkpoint $CHECKPOINT \
     --pre_seq_len $PRE_SEQ_LEN
-
